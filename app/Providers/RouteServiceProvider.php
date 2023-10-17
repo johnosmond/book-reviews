675f25eb6ed161c21b1208a726ce786a6dcbecf3
johnosmond <john@jtech10.com>
2023-10-17 13:42:09 -0400

    Final chapter from the tutorial. Review model, ReviewController class, show blade, rate limiting.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -37,4 +37,14 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/web.php'));
         });
     }
+
+    protected function configureRateLimiting(): void
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+        RateLimiter::for('reviews', function (Request $request) {
+            return Limit::perMinute(3)->by($request->user()?->id ?: $request->ip());
+        });
+    }
 }
