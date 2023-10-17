18d431baf83fcccf8254fb15476c9c6e6ba46d86
johnosmond <john@jtech10.com>
2023-10-16 23:24:32 -0400

    Adds caching. Adds rating component.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('books.index');
 });
+
+Route::resource('books', BookController::class);
