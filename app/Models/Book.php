0130df11c7a9b4c1af76853a9162cba51ac990d1
John Osmond <john@jtech10.com>
2024-04-23 23:01:14 -0400

    Modified scope methods to be more useful

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -19,4 +19,41 @@ class Book extends Model
     {
         return $query->where('title', 'LIKE', '%' . $title . '%');
     }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withCount([
+            'reviews' => fn (Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withAvg([
+            'reviews' => fn (Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    private function dateRangeFilter(Builder $query, $from = null, $to = null)
+    {
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        } elseif ($to && !$from) {
+            $query->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
+    }
+
+    public function scopeMinReviewsCount(Builder $query, int $count): Builder
+    {
+        return $query->withCount('reviews')
+            ->where('reviews_count', '>=', $count);
+    }
+
+    public function scopeMinRating(Builder $query, float $rating): Builder
+    {
+        return $query->withAvg('reviews', 'rating')
+            ->where('reviews_rating_avg', '>=', $rating);
+    }
 }
